73e0c8cab22419db824425ec106b7a76cbfeed42
Vitali Baranov <vitalib.freeman@gmail.com>
2022-09-24 11:32:54 +0300

    HW-3 enabel spring boot

--- src/main/java/by/baragvit/otus/homework/App.java
+++ src/main/java/by/baragvit/otus/homework/App.java
@@ -2,16 +2,18 @@ package by.baragvit.otus.homework;
 
 import by.baragvit.otus.homework.service.SimpleRunner;
 import by.baragvit.otus.homework.service.Runner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
-@ComponentScan
-@PropertySource("classpath:application.properties")
+
+@SpringBootApplication
 public class App {
   public static void main(String[] args) {
-    ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+    ApplicationContext context = SpringApplication.run(App.class);
     Runner runner = context.getBean(SimpleRunner.class);
     runner.run();
   }
